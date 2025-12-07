2bfa44ddf4d16a769368393d86acb4e2cbd8548f
Samira <shresthasamira2005@gmail.com>
2025-12-07 21:16:25 +0545

    making lab3 branch

--- lab2/js/q13.js
+++
@@ -1,7 +0,0 @@
-function joinArray() {
-    const myArray = ["Hello", "World", "JavaScript", "is", "fun"];
-    document.getElementById("unjoined").textContent = `Unjoined String: ${myArray}`;
-    const joinedString = myArray.join(" "); 
-    
-    document.getElementById("result").textContent = `Joined String: ${joinedString}`;
-}
