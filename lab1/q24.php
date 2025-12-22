f090991cd21334a02ae0cd12d597c2b6745504dc
Samira <shresthasamira2005@gmail.com>
2025-12-22 23:27:22 +0545

    q24 update

--- lab1/q24.php
+++ lab1/q24.php
@@ -1,16 +1,19 @@
 <?php
 function lastThreeUpper($str) {
     $length = strlen($str);
-    
+
     if ($length < 3) {
-        return strtoupper($str); 
-        $front = substr($str, 0, $length - 3); 
-        return $front . strtoupper($lastThree); 
+        return strtoupper($str);
     }
+
+    $front = substr($str, 0, $length - 3);
+    $lastThree = substr($str, -3);
+
+    return $front . strtoupper($lastThree);
 }
+
 $inputs = ["Nepal", "Npl", "Bca", "Bachelor"];
 foreach ($inputs as $input) {
-    $output = lastThreeUpper($input);
-    echo $output . "<br>";
+    echo lastThreeUpper($input) . "<br>";
 }
 ?>
